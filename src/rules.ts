129ee7b75110abf05108f877f00834683e1dd493
mablin7 <magyarb0201@gmail.com>
2021-06-21 02:26:41 +0300

    feat: add notebookPath and completed rules

--- src/rules.ts
+++ src/rules.ts
@@ -1,5 +1,5 @@
 import { Config } from "./board";
-import { getTagId, UpdateQuery } from "./noteData";
+import { getTagId, UpdateQuery, resolveNotebookPath } from "./noteData";
 
 export interface Rule {
   searchQueries: string[];
@@ -35,6 +35,64 @@ const rules: Rules = {
       ],
     };
   },
+
+  async tags(tagNames: string | string[], config: Config) {
+    if (!Array.isArray(tagNames)) tagNames = [tagNames];
+    const tagRules = await Promise.all(tagNames.map(t => rules.tag(t, config)))
+    return {
+      searchQueries: tagRules.flatMap(({ searchQueries }) => searchQueries),
+      set: (noteId: string) => tagRules.flatMap(({ set }) => set(noteId)),
+      unset: (noteId: string) => tagRules.flatMap(({ unset }) => unset(noteId))
+    }
+  },
+
+  async notebookPath(path: string | string[], config: Config) {
+    if (Array.isArray(path)) path = path[0];
+    const notebookId = await resolveNotebookPath(
+      path,
+      config.filters.rootNotebookPath
+    );
+    const notebook = path.split('/').pop()
+    return {
+      searchQueries: [`notebook:${notebook}`],
+      set: (noteId: string) => [
+        {
+          type: "put",
+          path: ["notes", noteId],
+          body: {
+            parent_id: notebookId,
+          },
+        },
+      ],
+      unset: () => [],
+    };
+  },
+
+  async completed(val: string | string[]) {
+    if (Array.isArray(val)) val = val[0];
+    const shouldBeCompeted = val.toLowerCase() === 'true'
+    return {
+      searchQueries: [`iscompleted:${shouldBeCompeted ? 1 : 0}`],
+      set: (noteId: string) => [
+        {
+          type: "put",
+          path: ["notes", noteId],
+          body: {
+            completed: shouldBeCompeted ? Date.now() : 0
+          },
+        },
+      ],
+      unset: (noteId: string) => [
+        {
+          type: "put",
+          path: ["notes", noteId],
+          body: {
+            completed: 0
+          },
+        }
+      ],
+    };
+  }
 };
 
 export default rules;
