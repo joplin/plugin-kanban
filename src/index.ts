522b95e0c56c1dd09790874bc597b50280bfa0cd
mablin7 <magyarb0201@gmail.com>
2021-06-20 19:13:47 +0300

    feat: create board object when a board note is opened

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
 import joplin from "api";
 
+import createBoard, { Board } from "./board";
+import { getConfigNote, searchNotes } from "./noteData";
+
+let openBoard: Board | undefined;
+
+function showBoard() {}
+
+function hideBoard() {}
+
+async function handleNewlyOpenedNote(newNoteId: string) {
+  if (openBoard) {
+    const allNotesOnBoard = await searchNotes(openBoard.allNotesQuery);
+    const containsOpenedNote = !!allNotesOnBoard.find(
+      ({ id }) => id === newNoteId
+    );
+
+    if (containsOpenedNote) return;
+    else {
+      hideBoard();
+      openBoard = undefined;
+    }
+  }
+
+  if (!openBoard || (openBoard as Board).configNoteId !== newNoteId) {
+    const note = await getConfigNote(newNoteId);
+    const board = await createBoard(note);
+    if (board) {
+      openBoard = board;
+      showBoard();
+    }
+  }
+}
+
 joplin.plugins.register({
   onStart: async function () {
-    console.info("Test plugin started!");
+    joplin.workspace.onNoteSelectionChange(
+      async ({ value }: { value: [string?] }) => {
+        const newNoteId = value?.[0];
+        if (newNoteId) handleNewlyOpenedNote(newNoteId);
+      }
+    );
   },
 });
